c9478544af7bde817b49c871f177399a57926d5c
Rahul Raj khare <rahulrajkhare@gmail.com>
2023-01-11 18:54:54 +0530

    Added basic auth-screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dating_made_better/screens/auth_screen.dart';
 import 'package:dating_made_better/screens/user_profile_overview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -24,12 +25,15 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Dating, made better!',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
         ),
-        home: const SwipingScreen(),
+        home: AuthScreen(),
         routes: {
+          AuthScreen.routeName: (context) => AuthScreen(),
           SwipingScreen.routeName: (context) => const SwipingScreen(),
-          UserProfileScreen.routeName: (context) => UserProfileScreen(),
+          UserProfileScreen.routeName: (context) => const UserProfileScreen(),
           UserProfileCompletionScreen.routeName: (context) =>
               UserProfileCompletionScreen(),
         },
---
+++ lib/providers/auth.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signup(String number, String OTP) async {
+
+  }
+}
