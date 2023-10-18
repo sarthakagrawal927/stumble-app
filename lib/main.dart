697013cf38098eefa0dace6806f354e0b3a9e200
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-10-19 00:54:02 +0530

    Add platform level errors to google analytics as well

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:dating_made_better/utils/call_api.dart';
 import 'package:dating_made_better/utils/helper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -41,6 +42,10 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   ChuckerFlutter.showOnRelease = true;
   runApp(const MyApp());
 }
