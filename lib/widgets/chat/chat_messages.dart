6231b2af98e4cf6d4c060240679abe73edaeba36
Rahul Raj khare <rahulrajkhare@gmail.com>
2023-11-15 23:28:02 +0530

    Scrolled to bottom hack cycle 5

--- lib/widgets/chat/chat_messages.dart
+++ lib/widgets/chat/chat_messages.dart
@@ -2,6 +2,7 @@ import 'package:dating_made_better/global_store.dart';
 import 'package:dating_made_better/models/chat.dart';
 import 'package:dating_made_better/widgets/chat/message_bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class ChatMessages extends StatelessWidget {
   final List<ChatMessage> listOfChatMessages;
@@ -12,8 +13,14 @@ class ChatMessages extends StatelessWidget {
   Widget build(BuildContext context) {
     int selfUserId = AppConstants.user["id"];
 
+    ScrollController scrollController = ScrollController();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      scrollController.jumpTo(scrollController.position.maxScrollExtent);
+    });
+
     return ListView.builder(
       reverse: false,
+      controller: scrollController,
       itemBuilder: ((context, index) => MessageBubble(
             message: listOfChatMessages[index].message,
             isMe: listOfChatMessages[index].senderId == selfUserId,
