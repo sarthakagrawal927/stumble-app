93e33aa67ca07895550c1c2238ebd9ef9747d0a5
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-11-18 13:24:03 +0530

    Revamp beautify Time function

--- lib/utils/general.dart
+++ lib/utils/general.dart
@@ -9,26 +9,21 @@ String beautifyTime(DateTime dateTime) {
   dateTime = dateTime.toLocal();
   DateTime now = DateTime.now();
   String amPm = dateTime.hour < 12 ? 'AM' : 'PM';
-  if (dateTime.year == now.year &&
-      dateTime.month == now.month &&
-      dateTime.day == now.day) {
-    // If it's today, return "hh:mm AM/PM" format
-    String formattedTime =
-        '${dateTime.hour % 12}:${dateTime.minute.toString().padLeft(2, '0')} $amPm';
-    return formattedTime;
-  } else if (dateTime.year == now.year &&
-      dateTime.month == now.month &&
-      dateTime.day == now.day - 1) {
-    // If it's yesterday, return "hh:mm AM/PM, yesterday" format
-    String formattedTime =
-        '${dateTime.hour % 12}:${dateTime.minute.toString().padLeft(2, '0')} $amPm';
-    return '$formattedTime, yesterday';
-  } else {
-    // For any other day, return "hh:mm AM/PM ddMM" format
-    String formattedTime =
-        '${dateTime.hour % 12}:${dateTime.minute.toString().padLeft(2, '0')} $amPm';
-    String formattedDate =
-        '${dateTime.day.toString().padLeft(2, '0')}${dateTime.month.toString().padLeft(2, '0')}';
-    return '$formattedTime $formattedDate';
+  int hour = dateTime.hour % 12;
+  if (hour == 0 && amPm == "PM") hour = 12;
+  String formattedTime =
+      '$hour:${dateTime.minute.toString().padLeft(2, '0')} $amPm';
+  // I know on 1st of month, yesterday's message will be shown with complete date, similarly with year
+  if (dateTime.year == now.year && dateTime.month == now.month) {
+    if (dateTime.day == now.day - 1) {
+      return '$formattedTime, yesterday';
+    }
+    if (dateTime.day == now.day) {
+      return formattedTime;
+    }
   }
+
+  String formattedDate =
+      '${dateTime.day.toString().padLeft(2, '0')}/${dateTime.month.toString().padLeft(2, '0')}';
+  return '$formattedTime, $formattedDate';
 }
