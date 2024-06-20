000393d463ba39ef7b724493302581647300fc9e
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-06-21 01:30:23 +0530

    Improve like icon

--- lib/widgets/comment_feature_widget.dart
+++ lib/widgets/comment_feature_widget.dart
@@ -21,57 +21,68 @@ class CommentFeatureWidget extends StatelessWidget {
         widget,
         Align(
             alignment: Alignment.topRight,
-            child: IconButton(
+            child: Padding(
               padding: EdgeInsets.only(
                 top: marginWidth64(context),
-                right: marginWidth24(context),
+                right: marginWidth64(context),
               ),
-              onPressed: () {
-                dialogWidget(
-                    context: context,
-                    submitLabel: "Like",
-                    onSubmit: () async {
-                      onSwipe(ActivityType.like, commentOnWidget);
-                      Navigator.of(context, rootNavigator: true).pop("");
-                    },
-                    childWidget: Column(
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height / 4,
-                          child: widget,
-                        ),
-                        TextField(
-                          maxLines: 2,
-                          minLines: 1,
-                          cursorColor: Colors.black,
-                          autocorrect: true,
-                          keyboardType: TextInputType.multiline,
-                          decoration: const InputDecoration(
-                            hintText: "Leave a compliment!",
-                            hintStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
+              child: IconButton(
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                  backgroundColor: MaterialStateProperty.all(
+                    AppColors.primaryColor,
+                  ),
+                ),
+                onPressed: () {
+                  dialogWidget(
+                      context: context,
+                      submitLabel: "Like",
+                      onSubmit: () async {
+                        onSwipe(ActivityType.like, commentOnWidget);
+                        Navigator.of(context, rootNavigator: true).pop("");
+                      },
+                      childWidget: Column(
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 4,
+                            child: widget,
+                          ),
+                          TextField(
+                            maxLines: 2,
+                            minLines: 1,
+                            cursorColor: Colors.black,
+                            autocorrect: true,
+                            autofocus: true,
+                            keyboardType: TextInputType.multiline,
+                            decoration: const InputDecoration(
+                              hintText: "Leave a compliment!",
+                              hintStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                              ),
                             ),
-                          ),
-                          textInputAction: TextInputAction.newline,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
-                          maxLength: 75,
-                          onChanged: (value) {
-                            commentOnWidget = value;
-                          },
-                        )
-                      ],
-                    ),
-                    title: "Like $name!");
-              },
-              icon: const Icon(
-                Icons.favorite_outline_rounded,
-                color: AppColors.primaryColor,
+                            textInputAction: TextInputAction.newline,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                            maxLength: 75,
+                            onChanged: (value) {
+                              commentOnWidget = value;
+                            },
+                          )
+                        ],
+                      ),
+                      title: "Like $name!");
+                },
+                icon: const Icon(
+                  Icons.favorite_rounded,
+                  applyTextScaling: true,
+                  color: AppColors.backgroundColor,
+                  semanticLabel: "Like the profile",
+                ),
+                iconSize: MediaQuery.of(context).size.width / 13,
               ),
-              iconSize: marginWidth12(context),
             ))
       ],
     );
